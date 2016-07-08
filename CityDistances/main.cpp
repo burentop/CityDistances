94ee92317882bd4ea67fcc03c4b4ce8fd6c496e7
Brent Perry <burentoperii@gmail.com>
2016-07-08 13:59:30 -0500

    Initial commit of CityDistances.

--- CityDistances/main.cpp
+++ CityDistances/main.cpp
@@ -6,10 +6,34 @@
 //  Copyright © 2016 Brent Perry. All rights reserved.
 //
 
-#include <iostream>
+#include "../../../std_lib_facilities.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+    vector<double> distances;
+    double low, high, mean;
+    double sum = 0;
+    
+    cout << "Please type distances between cities (enter any letter to quit): \n";
+    
+    for (double distance; cin >> distance;) {
+        distances.push_back(distance);
+    }
+    
+    sort(distances);
+    
+    low = distances[0];
+    high = distances[int (distances.size() - 1)];
+    
+    for (double distance : distances) {
+        sum += distance;
+    }
+    
+    mean = sum / distances.size();
+    
+    cout << "Shortest distance entered: " << low << ".\n";
+    cout << "Longest distance entered: " << high << ".\n";
+    cout << "Total distance: " << sum << ".\n";
+    cout << "Mean distance: " << mean << ".\n";
+    
 }
